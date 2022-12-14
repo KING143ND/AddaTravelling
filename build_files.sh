ec5ff6265ba1bf23ab6cf5a473bfb49997109f15
Naveen Chaudhary <94450038+KING143ND@users.noreply.github.com>
2022-12-14 14:02:28 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 echo " BUILD START"
-python3.10.5 manage.py makemigrations
-python3.10.5 manage.py migrate
 python3.10.5 -m pip install -r requirements.txt
 python3.10.5 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
