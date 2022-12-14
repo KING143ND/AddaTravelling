e5ce474f87c5eca8a56a4ae699c6637aed152c46
Naveen Chaudhary <94450038+KING143ND@users.noreply.github.com>
2022-12-14 15:17:29 +0530

    vercel again12

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.10.5 -m pip install -r requirements.txt
+python3.10.5 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
 
 
